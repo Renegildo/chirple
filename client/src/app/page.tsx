83d8712eadc28a0dc65f2af018015dc724d2166b
Renegildo <renegildo@proton.me>
2024-07-25 08:12:40 -0300

    feat: removed the test page from the root of the website

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,45 +1,8 @@
 "use client";
 
-import { useEffect } from 'react';
-import { socket } from '../socket';
-
 export default function Home() {
-  const self = {
-    id: "123897123",
-    username: "renegildo",
-  }
-
-  const roomId = 100;
-
-  const handleSendMessage = () => {
-    socket.emit("sendMessage", {
-      owner: self,
-      roomId: roomId,
-      message: "Opa eai kk",
-    });
-  }
-
-  useEffect(() => {
-    socket.emit("joinRoom", roomId);
-    console.log("joining room...");
-
-    socket.on("message", (data) => {
-      alert(JSON.stringify(data));
-    });
-
-    return () => {
-      socket.off("message");
-    };
-  }, []);
-
   return (
     <div>
-      <h1>
-        Testando
-      </h1>
-      <button type='button' onClick={handleSendMessage}>
-        Send Message
-      </button>
     </div>
   );
 }
