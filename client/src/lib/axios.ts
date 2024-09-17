1771328ff1eafb64206d1f9201aeb1e51e0f003e
Renegildo <renegildo@proton.me>
2024-09-17 14:34:12 -0300

    fix: update baseUrl in axios api config

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,6 @@
-import { baseServerUrl } from '@/utils/constants';
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: baseServerUrl,
+  baseURL: "chirple-api.onrender.com",
   withCredentials: true,
 });
