a741674a8eacab5ffe23734ac9e494f30f439ae8
Renegildo <renegildo@proton.me>
2024-09-17 13:42:23 -0300

    fix: update base server url

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const baseServerUrl = process.env.NODE_ENV === "production" ? "http://100.65.32.8" : "http://192.168.100.123:8080"
+export const baseServerUrl = process.env.NODE_ENV === "production" ? "https://chirple-api.onrender.com" : "http://192.168.100.123:8080"
