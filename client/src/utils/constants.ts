4d307db079be374813a3081d75cd3c28a9605b29
renegildo <renegildo@proton.me>
2024-10-30 17:16:27 -0300

    fix: update baseServerUrl to match the new /api route

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,2 +1,2 @@
 // export const baseServerUrl = process.env.NODE_ENV === "production" ? "https://chirple-api.onrender.com" : "http://192.168.100.123:8080"
-export const baseServerUrl = "192.168.100.101";
+export const baseServerUrl = "http://192.168.100.101/api";
