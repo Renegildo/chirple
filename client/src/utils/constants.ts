90d758032edfe2cfedf9d552f8c3370b66e1efb5
renegildo <renegildo@proton.me>
2024-10-30 16:53:33 -0300

    add: add docker-compose.yml and update baseServerUrl in front end

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const baseServerUrl = process.env.NODE_ENV === "production" ? "https://chirple-api.onrender.com" : "http://192.168.100.123:8080"
+// export const baseServerUrl = process.env.NODE_ENV === "production" ? "https://chirple-api.onrender.com" : "http://192.168.100.123:8080"
+export const baseServerUrl = "192.168.100.101";
